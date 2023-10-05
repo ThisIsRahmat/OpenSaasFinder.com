3cca8562e072ce9986d6ec21d36d5304a7ca0581
thisisrahmat <thisisrahmat@gmail.com>
2023-10-05 23:54:00 +0100

    added serversideprops to fix deploy

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,28 +4,26 @@ import Hero from '../components/Hero'
 import Search from '../components/Search'
 import NewSearch from '../components/NewSearch'
 import DisplayTypes from '../components/DisplayTypes'
-// import { getOpensaas } from "../lib/getSaas"
+import { getOpensaas } from "../lib/getSaas"
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
 
-
-
-export default async function Home({ searchParams: string }) {
-
-  const searchText = searchParams.saas;
-
-
+export default function Home({ searchText }) {
 
   return (
-   
-        
     <main className="flex sm:h-screen flex-col items-center space-y-10  p-24">
-   
-   <Hero/>
-   {/* <DisplayTypes/> */}
-
-  <NewSearch searchText={searchText}/>
+      <Hero/>
+      <NewSearch searchText={searchText}/>
     </main>
-
   )
+}
+
+export async function GetServerSideProps({searchParams}) {
+  const searchText = searchParams.saas;
+  return {
+    props: {
+      searchText: searchParams.saas
+    }
+  }
 }
