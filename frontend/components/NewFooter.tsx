7b257d96d3e76a3a67f60c879faf5bdea98ac7f5
thisisrahmat <thisisrahmat@gmail.com>
2023-10-06 16:24:14 +0100

    removed dark from header and footer

--- frontend/components/NewFooter.tsx
+++ frontend/components/NewFooter.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link'
 
 export default function NewFooter() {
 return (
-    <footer className="p-4  bg-white sm:p-6 dark:bg-gray-800">
+    <footer className="p-4  bg-white sm:p-6 ">
     <div className="mx-auto max-w-screen-xl">
 
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex ">
         
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
