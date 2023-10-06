bf1432b598c4e5449cf43b0fae9d2add82f6746b
thisisrahmat <thisisrahmat@gmail.com>
2023-10-06 02:40:25 +0100

    trying to fix handleSubmit type

--- frontend/components/NewSearch.tsx
+++ frontend/components/NewSearch.tsx
@@ -8,12 +8,13 @@ export default function NewSearch({ searchText = " " }){
 
     const router = useRouter()
 
-    function handleSubmit(e :React.FormEvent<HTMLInputElement>){
+    function handleSubmit(e:  React.ChangeEventHandler<HTMLInputElement>){
       e.preventDefault();
-      const form = e.target;
+      const form = e.currentTarget;
       //replace any whitespaces at start of the search string
       const saasValue = form.saas.value.trimStart();
-    console.log(`/saas/${saasValue}`)
+    console.log(e)
+    console.log(form.value)
       router.push(`/saas/${saasValue}`)
   
     }
@@ -22,9 +23,11 @@ export default function NewSearch({ searchText = " " }){
         <main className=" dark:bg-gray-900 flex items-center">
           <div className="max-w-screen-xl px-4 mx-auto lg:px-12 w-full">
             <div className="relative dark:bg-gray-800 sm:rounded-lg">
-              <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
-                <div className="w-full md:w-1/2">
+            
+                
                   <form className="flex items-center" onSubmit={handleSubmit}>
+                  <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
+                  <div className="w-full md:w-1/2">
                     <label htmlFor="simple-search" className="sr-only">Search</label>
                     <div className="relative w-full">
                       <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -45,23 +48,21 @@ export default function NewSearch({ searchText = " " }){
 
         />
                     </div>
-                  </form>
-                </div>
-                <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
-                  <button type="button" className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white  bg-black   focus:outline-none ">
+                    </div>
+
+                    <div className="flex flex-col items-stretch justify-end flex-shrink-0 w-full space-y-2 md:w-auto md:flex-row md:space-y-0 md:items-center md:space-x-3">
+                  <button type="submit" className="flex items-center justify-center px-4 py-2 text-sm font-medium text-white  bg-black   focus:outline-none ">
                   
                    Search
                   </button>
-                  <div className="flex items-center w-full space-x-3 md:w-auto">
-                   
-                    <div id="actionsDropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                      
-                   
+                
+                </div> 
+                    
                     </div>
-              
-                  </div>
-                </div>
-              </div>
+                    
+                  </form>
+
+        
             </div>
           </div>
         </main>
