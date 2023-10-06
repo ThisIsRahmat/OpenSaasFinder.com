4c4b41d55cbb4a455df159fe6467418b3f4f270c
thisisrahmat <thisisrahmat@gmail.com>
2023-10-06 03:29:40 +0100

    fixed deployment error on types

--- frontend/components/DisplayTypes.tsx
+++ frontend/components/DisplayTypes.tsx
@@ -1,31 +1,31 @@
-import Link from 'next/link';
-import { displayType } from "../lib/displayType";
+// import Link from 'next/link';
+// import { displayType } from "../lib/displayType";
 
-export default function DisplayType(){
+// export default function DisplayType(){
 
-    const dataTypes: string[] = []
+//     const dataTypes: string[] = []
     
 
-  const listTypes = displayType();
+//   const listTypes = displayType();
  
-console.log(listTypes)
+// console.log(listTypes)
 
 
-    return (
-      <main>
-        {/* <div>
-          {listTypes.map((t, index) => ( 
-            <ul key={index}>
-              <Link href={`/${t.name}`}>
-                <li>{t.name}</li>
-              </Link>
-            </ul>
-          ))}
-        </div> */}
-        <p> These are all the listTypes</p>
-        {listTypes}
-        <br/>
+//     return (
+//       <main>
+//         {/* <div>
+//           {listTypes.map((t, index) => ( 
+//             <ul key={index}>
+//               <Link href={`/${t.name}`}>
+//                 <li>{t.name}</li>
+//               </Link>
+//             </ul>
+//           ))}
+//         </div> */}
+//         <p> These are all the listTypes</p>
+//         {listTypes}
+//         <br/>
 
-      </main>
-    );
-}
+//       </main>
+//     );
+// }
